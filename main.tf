9f16e8a769078fb25304f77a799fec5930e1b1a8
Pavel Kratochvil <pavel.kratochvil@tietoevry.com>
2021-08-10 16:32:03 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,120 @@
+provider "proxmox" {
+    pm_api_url = var.proxmox_host["pm_api_url"]
+    pm_user = var.proxmox_host["pm_user"]
+    pm_tls_insecure = true
+    pm_password="TietoEVRY2021"
+    pm_parallel = 10
+}
+
+resource "proxmox_vm_qemu" "k3s-server" {
+  count = length(var.k3s-server-hostnames)
+  name = var.k3s-server-hostnames[count.index]
+  target_node = var.proxmox_host["target_node"]
+  vmid = var.k3s-server-ids + count.index
+  
+  clone = "clone"
+  clone_wait = 50
+  full_clone = true
+
+  cores = 4
+  sockets = 1
+  memory = 16384
+  os_type="cloud-init"
+  scsihw = "virtio-scsi-pci"
+  
+  network {
+    bridge = "vmbr1"
+    model = "virtio"
+  }
+  
+  ipconfig0 = "ip=${var.k3s-server-ips[count.index]}/24,gw=${cidrhost(format("%s/24", var.k3s-server-ips[count.index]), 1)}"
+  
+  disk {
+    type = "virtio"
+    storage = "local"
+    size = "4G"
+  }
+  
+  connection {
+    host = var.k3s-server-ips[count.index]
+    user = var.user
+    private_key = file(var.ssh_keys["priv"])
+  }
+}
+
+resource "proxmox_vm_qemu" "k3s-agent-node" {
+  count = length(var.k3s-node-hostnames)
+  name = var.k3s-node-hostnames[count.index]
+  target_node = var.proxmox_host["target_node"]
+  vmid = var.k3s-node-ids + count.index
+  
+  clone = "clone"
+  clone_wait = 50
+  full_clone = true
+
+  cores = 4
+  sockets = 1
+  memory = 16384
+  os_type="cloud-init"
+  scsihw = "virtio-scsi-pci"
+  
+  network {
+    bridge = "vmbr1"
+    model = "virtio"
+  }
+  
+  ipconfig0 = "ip=${var.k3s-node-ips[count.index]}/24,gw=${cidrhost(format("%s/24", var.k3s-node-ips[count.index]), 1)}"
+  
+  disk {
+    type = "virtio"
+    storage = "local"
+    size = "4G"
+  }
+  
+  connection {
+    host = var.k3s-node-ips[count.index]
+    user = var.user
+    private_key = file(var.ssh_keys["priv"])
+  }
+}
+
+
+/*
+resource "proxmox_vm_qemu" "k3s-load-balancer" {
+  count = length(var.hostnames)
+  name = var.hostnames[count.index]
+  target_node = var.proxmox_host["target_node"]
+  vmid = var.vmid + count.index
+  
+  clone = "clone"
+  clone_wait = 30
+  full_clone = true
+
+  cores = 4
+  sockets = 1
+  vcpus = 2
+  memory = 16384
+  os_type="cloud-init"
+  scsihw = "virtio-scsi-pci"
+  
+  network {
+    name="eth0"
+    bridge="vmbr0"
+    ip="dhcp"
+  }
+  
+  // ipconfig0 = "ip=${var.ips[count.index]}/24,gw=${cidrhost(format("%s/24", var.ips[count.index]), 1)}"
+  
+  disk {
+    type = "virtio"
+    storage = "local"
+    size = "12G"
+  }
+  
+  connection {
+    host = var.ips[count.index]
+    user = var.user
+    private_key = file(var.ssh_keys["priv"])
+  }
+}
+*/
---
+++ var.tf
@@ -0,0 +1,70 @@
+variable "proxmox_host" {
+	type = map
+    default = {
+      pm_api_url = "https://localhost:20001/api2/json"
+      pm_user = "root@pam"
+      target_node = "pve-01"
+    }
+}
+
+/*
+variable "machine" {
+  // TODO: fill in information
+  // cores
+  // sockets
+  // ...
+}
+*/
+
+
+variable "k3s-server-ids" {
+	description = "Starting ID for k3s servers"
+  default     = 105
+}
+
+variable "k3s-node-ids" {
+	description = "Starting ID for the k3s agent nodes"
+  default     = 205
+}
+
+variable "k3s-server-hostnames" {
+  description = "Servers to be created"
+  type        = list(string)
+  default     = ["k3s-server1", "k3s-server2"]
+}
+
+variable "k3s-node-hostnames" {
+  description = "Worker nodes to be created"
+  type        = list(string)
+  default     = ["k3s-node1", "k3s-node2", "k3s-node3", "k3s-node4"]
+}
+
+variable "k3s-server-ips" {
+  description = "IPs of the servers, respective to the hostname order"
+  type        = list(string)
+  default     = ["10.0.0.40", "10.0.0.41"]
+}
+
+variable "k3s-node-ips" {
+  description = "IPs of the nodes, respective to the hostname order"
+  type        = list(string)
+  default     = ["10.0.0.42", "10.0.0.43", "10.0.0.44", "10.0.0.45"]
+}
+
+// Config variables for Ansible
+variable "ssh_keys" {
+	type = map
+    default = {
+      pub  = "~/.ssh/id_rsa.pub"
+      priv = "~/.ssh/id_rsa"
+    } 
+}
+
+variable "user" {
+	default     = "root"
+	description = "User used to SSH into the machine and provision it"
+}
+
+variable "rootfs_size" {
+	default = "4G"
+}
